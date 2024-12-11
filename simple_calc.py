3082bf31b581125373b7b7ec6317a3b6555f230a
Niccolò Marastoni <niccolo.marastoni@gmail.com>
2024-12-11 09:59:19 +0100

    dividing the program into files

--- simple_calc.py
+++ simple_calc.py
@@ -1,6 +1,8 @@
-a = int(input('a = '))
-b = int(input('b = '))
-if a > b:
-    print(a + b)
-else:
-    print(a - b)
+def sum(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mult(a, b):
+    return a*b
