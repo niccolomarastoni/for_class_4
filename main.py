bb19edc88978194844a8bd6d2b10ecb43140121e
Niccolò Marastoni <niccolo.marastoni@gmail.com>
2024-12-11 10:03:35 +0100

    added sum to the main program

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import simple_calc as sc
+
 if __name__ == '__main__':
-    print('it works')
+    op = int(input('what operation do you need?\n0 - sum, 1 - sub, 2 - mult'))
+    a = int(input('a = '))
+    b = int(input('b = '))
+    if op == 0:
+        print(f'{a} + {b} = {sc.sum(a, b)}')
+
+    else:
+        print('not available yet')
